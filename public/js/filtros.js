2c54e825dc387db3d5cb083de1acb7a3eaeeaecf
josueeegl <josuegl12@outlook.com>
2021-09-14 20:20:26 -0600

    v6

--- public/js/filtros.js
+++ public/js/filtros.js
@@ -1,9 +1,9 @@
 var c = 0;
 //mostrar/ocultar ventana de filtros
 function filtros() {
-
     if (c == 0) {
         document.getElementById('filtros').style.display = 'block';
+        window.scrollTo(0, 160);
         c = 1;
     } else {
         document.getElementById('filtros').style.display = 'none';
@@ -28,22 +28,20 @@ var myDate = new Date();
 var año = myDate.getFullYear();
 var year = document.getElementById('fYear');
 const llenar_select = () => {
-    for (var i = 1940; i < año + 1; i++) {
-        year.innerHTML += `<option value="${i}" selected="selected">${i}
+    for (var i = año; i > 1940 + 1; i--) {
+        year.innerHTML += `<option value="${i}">${i}
         </option>`;
     }
 }
 llenar_select();
 
-
+//////////////////////////////////////////////////////////////////////////////////
 
 // INICIO de boton GUARDAR
 const aplicar_cambios = () => {
     filtros();
     aplicando_cambios();
 }
-
-
 var solicitud_filtro;
 var main0 = document.querySelector('.main').innerHTML;
 var verificar_generos = 0;
@@ -51,28 +49,29 @@ var verificar = 0;
 var arreglo = {
     genres: []
 };
+
 // evaluar tipo de contenido 
 const evaluar_tipo = () => {
-    var valor = 'pelis';
+    var valor = 'pelis'; //valor por defecto pelis
     var value_tipo = document.getElementsByName('tipo');
     for (let i = 0; i < value_tipo.length; i++) {
-        if (value_tipo[i].checked) {
+        if (value_tipo[i].checked) { //Si se ha seleccionado pelicula o serie
             valor = value_tipo[i].value;
             verificar = 1;
         }
     }
-    if (valor == 'pelis') {
+    if (valor == 'pelis') { // si no se ha seleccionado nada se aplicara para peliculas
         solicitud_filtro = api_base + movie_genres_http;
     } else if (valor == 'series') {
         solicitud_filtro = api_base + tv_genres_http;
     }
-    console.log(solicitud_filtro);
 }
 
 //evaluamos todos los checkbox de categorias
 const evaluar_checkbox_categorias = (data) => {
     var c = 0;
     data.forEach((element) => {
+        //Si se selecciona un genere se llena el array con los generps seleccionados
         if (document.getElementById(`${element.name+element.id}`).checked) {
             verificar_generos = 1;
             verificar = 1;
@@ -84,6 +83,29 @@ const evaluar_checkbox_categorias = (data) => {
         c++;
     });
 }
+
+// Año seleccionado
+const por_fecha = () => {
+    var value_year = document.getElementById('fYear').value;
+    if (value_year != 'year') {
+        solicitud_filtro += `&primary_release_year=${value_year}`;
+        verificar = 1;
+    }
+}
+
+//Clasificacion para adultos
+const para_adultos = () => {
+    if (document.getElementById('fadulto').checked) {
+        solicitud_filtro += '&include_adult=true';
+        verificar = 1;
+    }
+}
+
+
+
+
+
+
 // Está funcion aplicara todos los cambios elegidos en la seccion filtros
 const aplicando_cambios = () => {
     arreglo = {
@@ -92,12 +114,14 @@ const aplicando_cambios = () => {
     solicitud_filtro = '';
     evaluar_tipo();
     evaluar_checkbox_categorias(data_genres);
+    solicitud_filtro += new URLSearchParams({
+        api_key: api_key,
+        page: Math.floor(Math.random() * 3) + 1,
+        language: lenguage
+    });
+    por_fecha();
+    para_adultos();
     if (verificar == 1) {
-        solicitud_filtro += new URLSearchParams({
-            api_key: api_key,
-            page: Math.floor(Math.random() * 3) + 1,
-            language: lenguage
-        });
         document.querySelector('.main').innerHTML = main0;
         if (verificar_generos == 1) {
             recorrer(arreglo.genres);
@@ -107,7 +131,9 @@ const aplicando_cambios = () => {
     }
 }
 
+// Aca se recorre el arreglo para imprimir en pantalla
 const recorrer = (data) => {
+    console.log(solicitud_filtro);
     data.forEach(element => {
         fetchMovieListByGenres(solicitud_filtro, element.id, element.name);
     });
